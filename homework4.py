3211685ea9d188ba55a69070d8ff963382163436
kevin00744 <52673800+kevin00744@users.noreply.github.com>
2023-03-09 02:29:10 +0000

    modified:   homework4.py 	modified:   homework1.py

--- homework4.py
+++ homework4.py
@@ -1,7 +1,9 @@
+# 自行輸入 row , column , grid size,預設為3, 4, 2
 row = int(input('input the row number\n>'))
 column = int(input('input the column number\n>'))
 size = int(input('input the grid size\n>'))
 def sep():
+    # 進行第一行分隔好之運算, 第一個'＋'自行輸出 '-'數量受到size影響 ' + '數量 = row + 1 因此以下回圈進行判定
     print ('+', sep = '', end = '')
     for x in range (row):
         for y in range (size):
